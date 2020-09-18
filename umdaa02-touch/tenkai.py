0a7279e17d7413832e3b52bb964b111177b891f8
OtakeTomomi <s1632033fs@s.chibakoudai.jp>
2020-09-19 01:24:34 +0900

    Initial commit

---
+++ umdaa02-touch/tenkai.py
@@ -0,0 +1,10 @@
+import pickle
+import pandas as pd
+
+
+# df.to_pickle('TrainEventDictionary_70.pkl')
+
+df_from_pkl = pd.read_pickle('TrainEventDictionary_70.pkl')
+
+print(df_from_pkl)
+print(df_from_pkl.index)
