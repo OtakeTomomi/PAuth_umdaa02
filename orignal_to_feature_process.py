72bd1ed41859df693d9de2136af988f9ea1e9279
OtakeTomomi <s1632033fs@s.chibakoudai.jp>
2020-09-23 18:39:31 +0900

    一通り終了→エラー対処中

--- orignal_to_feature_process.py
+++ orignal_to_feature_process.py
@@ -34,6 +34,7 @@ feature descriptors
 import pandas as pd
 import numpy as np
 import math
+from scipy import signal
 
 # read data
 df = pd.read_csv('orignal_data/data_event_change.csv')
@@ -45,7 +46,7 @@ def preprocess(df):
     # convert ms to s
     df2.loc[:, 'eventTime'] = df2.loc[:, 'eventTime']/1000
 
-    # flip sign of yaxis
+    # flip sign of y_axis
     df2.loc[:, 'positionY'] = -df2.loc[:, 'positionY']
 
     # start counting from 1
@@ -62,13 +63,13 @@ def preprocess(df):
     # downInd = [downInd; size(t, 1)]; # よくわからん
 
     # init feature output
-    Nfeat = 35
+    Nfeat = 30
     featMat = np.zeros([Nstrokes, Nfeat])
     featMat[:, :] = np.nan
 
     print(f'extracting features of  {Nstrokes}  strokes..')
 
-    # TODO: 実際は5→Nstrokesだがmatlabとインデックスの違いがありそうなので要確認
+    # 実際は5→Nstrokesだがmatlabとインデックスの違いがありそうなので要確認
     # extract features from each individual stroke
     for i in range(5):
         # strokeInd = df.loc[downInd(i):downInd(i + 1) - 1, :]
@@ -84,31 +85,28 @@ def preprocess(df):
 
         # id stuff (do not use as features!)
         # user id
-        featMat[i, 1] = x_stroke.iloc[1, 3]
+        featMat[i, 1] = x_stroke.iloc[0, 3]
         # session id
-        featMat[i, 2] = x_stroke.iloc[1, 4]
+        featMat[i, 2] = x_stroke.iloc[0, 4]
 
-        # TODO: 動作確認未完了
         # features
         # convert from pixels to : ピクセルからmmへの変換
         # x_stroke[:, ['positionX', 'positionY']] = pixTommFac(featMat[i, 13]) * x_stroke[:, ['positionX', 'positionY'])
         x_stroke.loc[:, 'positionX'] = (1/445) * x_stroke.loc[:, 'positionX'] * 25.4
         x_stroke.loc[:, 'positionY'] = (1/445) * x_stroke.loc[:, 'positionY'] * 25.4
 
-        # TODO: 条件と計算式の見直し
-        # FIXME:　matlabの仕様から直せていないので修正するが一旦放置
-        # time to next stroke (0 if last stroke in dataset) : 次のストロークまでの時間 (データセット内の最後のストロークの場合は 0)
-        # if last stroke in dset or last stroke of this user, set to Nan :
+        # TODO: 動作確認未完了
+        # time to next stroke (0 if last stroke in data_set) : 次のストロークまでの時間 (データセット内の最後のストロークの場合は 0)
+        # if last stroke in data_set or last stroke of this user, set to Nan :
         # データセット内の一番最後もしくはユーザのなかでの最後のデータの場合は　NaN を代入
-        if featMat[i, 3] == 0 or df2[downInd[i + 1], 'USER'] != featMat.iloc[i, 1]:
+        if featMat[i, 3] == 0 or df2.loc[downInd[i+1], 'USER'] != featMat[i, 1]:
             featMat[i, 3] = 'NaN'
         else:
-            featMat[i, 3] = df2(min([downInd[Nstrokes], downInd[i + 1]]), 'eventTime') - x_stroke.iloc[0, 2]
+            featMat[i, 3] = df2.loc[min([downInd[Nstrokes], downInd[i + 1]]), 'eventTime'] - x_stroke.iloc[0, 2]
 
         # time to last point of this stroke : このストロークの最後のポイントまでの時間
         featMat[i, 4] = x_stroke.iloc[-1, 2] - x_stroke.iloc[0, 2]
 
-        # TODO: 動作確認未完了
         # x-pos start
         featMat[i, 5] = x_stroke.iloc[0, 7]
         # y-pos start
@@ -118,23 +116,22 @@ def preprocess(df):
         # y-pos start
         featMat[i, 8] = x_stroke.iloc[-1, 8]
 
-        # TODO: 動作確認未完了
         # full dist : 完全な歪み
-        featMat[i, 9] = np.sqrt((featMat[i, 8] - featMat[i, 6]) ^ 2 + (featMat[i, 7] - featMat[i, 5]) ^ 2)
+        featMat[i, 9] = np.sqrt((featMat[i, 8] - featMat[i, 6]) ** 2 + (featMat[i, 7] - featMat[i, 5]) ** 2)
 
-        # FIXME: matlabのfilter関数の調査とpythonとの違いの調査後に修正
+        # TODO: 動作確認未完了
         # pairwise stuff→わからんので調査
         # x-displacement : X-変位
-        xdispl = filter([1 - 1], 1, x_stroke.iloc[:, 7])
-        xdispl[1] = []
+        xdispl = signal.lfilter(x_stroke[0:-1], 1, x_stroke.iloc[:, 7])
+        # xdispl[1] = []
         # y-displacement : Y-変位
-        ydispl = filter([1 - 1], 1, x_stroke.iloc[:, 8])
-        ydispl[1] = []
+        ydispl = signal.lfilter(x_stroke[0:-1], 1, x_stroke.iloc[:, 8])
+        # ydispl[1] = []
 
-        # FIXME: matlabのfilter関数の調査とpythonとの違いの調査後に修正
+        # TODO: 動作確認未完了
         # pairwise time diffs : ペアワイズ時間の差分
-        tdelta = filter([1 - 1], 1, x_stroke[:, 2])
-        tdelta[1] = []
+        tdelta = signal.lfilter(x_stroke[0:-1], 1, x_stroke[:, 2])
+        # tdelta[1] = []
 
         # TODO: 動作確認未完了
         # pairw angle　: ペアの角度
@@ -155,16 +152,16 @@ def preprocess(df):
         # pairwDist = sqrt(xdispl.^2 + ydispl.^2);
         pairwDist = math.hypot(xdispl, ydispl)
 
-        # FIXME: matlabのfilter関数の調査とpythonとの違いの調査後に修正
+        # TODO: 動作確認未完了
         # speed histogram : 速度ヒストグラム
         v = pairwDist / tdelta
-        featMat[i, 14: 16] = np.percentile(v, indivPrctlVals, interpolation='midpoint')
+        featMat[i, 14:17] = np.percentile(v, indivPrctlVals, interpolation='midpoint')
 
-        # FIXME: matlabのfilter関数の調査とpythonとの違いの調査後に修正
+        # TODO: 動作確認未完了
         # acceleration histogram : 加速度ヒストグラム
-        a = filter([1 - 1], 1, v)
+        a = signal.lfilter(v[0:-1], 1, v)
         a = a / tdelta
-        a[1] = []
+        # a[1] = []
 
         # TODO: 動作確認未完了
         # full stat stuff
@@ -174,21 +171,21 @@ def preprocess(df):
 
         # TODO: 未修正
         # median velocity of last 3 points : 最後の3点の中央値速度
-        featMat[i, 20] = math.median(v(max([end - 3,1]):end))
+        featMat[i, 20] = np.median(v[max(-4, 0):-1])
 
         # TODO: 動作確認未完了
         # max dist. beween direct line and true line (with sign):直線と真線の最大距離（符号あり
         xvek = x_stroke.iloc[:, 7] - x_stroke.iloc[0, 7]
         yvek = x_stroke.iloc[:, 8] - x_stroke.iloc[0, 8]
 
-        # TODO: 動作確認未完了
+        # TODO: 不明
         # project each vector on straight line : 各ベクトルを直線上に投影
         # compute unit line perpendicular to straight connection and project on this
         # 直線接続に垂直な単位線を計算して、投影
         perVek = np.cross([xvek[-1], yvek[-1], 0], [0, 0, 1])
-        perVek = perVek / np.sqrt(([perVek(1), perVek(2)] * [perVek(1), perVek(2)]))
+        perVek = perVek / np.sqrt([perVek[0], perVek[1]] * [perVek[0], perVek[1]])
         perVek[math.isnan(perVek)] = 0
-        # happens if vectors have lenght 0
+        # happens if vectors have length 0
 
         # TODO: 動作確認未完了
         # all distances to direct line:直線までの全距離
@@ -198,8 +195,8 @@ def preprocess(df):
         # TODO: findの調査
         # report maximal (absolute) distance: 最大(絶対)距離を報告
         absProj = abs(projectOnPerpStraight)
-        maxind = find(absProj == max(absProj))
-        featMat[i, 21] = projectOnPerpStraight(maxind(1))
+        maxind = np.where(absProj == max(absProj))
+        featMat[i, 21] = projectOnPerpStraight(maxind[0])
 
         # TODO: 動作確認未完了
         # stat of distances (bins are not the same for all strokes):距離の統計量 (ビンはすべてのストロークで同じではない)
@@ -211,7 +208,7 @@ def preprocess(df):
 
         # TODO: 動作確認未完了
         # direction of end-to-end line: 端から端までの直線の方向
-        featMat[i, 12] = np.atan2((featMat.iloc[i, 8] - featMat.iloc[i, 6]), (featMat.iloc[i, 7] - featMat.iloc[i, 5]))
+        featMat[i, 12] = np.atan2((featMat[i, 8] - featMat[i, 6]), (featMat[i, 7] - featMat[i, 5]))
 
         # direction flag 1 up, 2 down, 3 left 4 right  (see doc atan2): in what direction is screen being moved to?
         # 方向フラグ 1 上向き，2 下向き，3 左向き，4 右向き:画面はどの方向に移動しているか
@@ -244,40 +241,26 @@ def preprocess(df):
 
         # TODO: 動作確認未完了
         # ratio between direct length and length of trajectory : 直接の長さと軌道の長さの比
-        featMat[i, 27] = featMat.iloc[i, 9] / featMat.iloc[i, 26]
+        featMat[i, 27] = featMat[i, 9] / featMat[i, 26]
 
         # TODO: 動作確認未完了
         # average velocity : 平均速度
-        featMat[i, 28] = featMat.iloc[i, 26] / featMat.iloc[i, 4]
+        featMat[i, 28] = featMat[i, 26] / featMat[i, 4]
 
         # TODO: 動作確認未完了
         # average acc over first 5 points : 最初の 5 ポイントの平均アクセント
-        featMat[i, 29] = math.median(a[0:min(5,len(a))])
+        featMat[i, 29] = np.median(a[0:min(5, len(a))])
 
         # TODO: 動作確認未完了
         # pressure in the middle of the stroke: ストロークの途中の圧力
-        featMat[i, 30] = math.median(x_stroke.loc[np.floor(npoints / 2):np.ceil(npoints / 2), 'eventPressure'])
-
-        # 要らない
-        '''
-        # covered area in the middle of the stroke:ストロークの途中でカバーされた領域
-        # featMat[i, 31] = math.median(x_stroke[np.floor(npoints / 2):np.ceil(npoints / 2), 'col_area'] )
-
-        # finger orientation in the middle of the stroke: ストロークの中央での指の向き
-        # featMat[i, 32] = math.median(x_stroke[np.floor(npoints/2):np.ceil(npoints/2), 'col_Forient'])
-
-        # change of finger orientation during stroke: ストローク中の指の向きの変化
-        # featMat[i, 33] =  x_stroke.loc[-1, 'col_Forient'] - x_stroke.loc[0, 'col_Forient']
-
-        # phone orientation: 電話の向き
-        # featMat[i, 34] =  x_stroke.loc[0 ,'col_orient']
-        '''
+        featMat[i, 30] = np.median(x_stroke.loc[np.floor(npoints / 2):np.ceil(npoints / 2), 'eventPressure'])
 
     print(df.head())
     print(downInd[:10])
     print(pd.DataFrame(featMat).head())
 
     # delete NaNs
-    # featMat(isnan(featMat(:, 1)),:) = []
+    # featMat(np.isnan(featMat(:, 1)),:) = []
+
 
 preprocess(df)
